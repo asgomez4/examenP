1c9456379b91506b9148fbccf28fc6b5356a86aa
Sofia Gomez <Sofia Gomez@Sofy>
2017-07-12 21:11:24 -0500

    Format a la clase

--- src/main/java/ec/edu/espe/isi/examen/examenprac/Operaciones.java
+++ src/main/java/ec/edu/espe/isi/examen/examenprac/Operaciones.java
@@ -9,7 +9,8 @@ package ec.edu.espe.isi.examen.examenprac;
  *
  * @author sony
  */
-public class Operaciones {    
+public class Operaciones {
+
     private int num1;
     private int num2;
     private int resultado;
@@ -18,62 +19,68 @@ public class Operaciones {
         this.num1 = num1;
         this.num2 = num2;
     }
+
     /**
      * Funcion que realiza la suma de dos números
+     *
      * @return devuelve el resultado de la suma
      */
-   public int suma(){
-       return num1 + num2;
-   }
-   /**
-    * Funcion que realiza la resta de dos números
-    * @return devuelve el resultado de la resta
-    */
-   public int resta(){
-       return num1 - num2;
-   }
-   /**
-    * metodo que devuelve la multiplicacion entre dos numeros
-    * @return multiplicacion
-    */
-   public int multiplicacion(){
-       return num1 * num2;
-   }
-   
-   /**
-    * metodo que devuelve la division entre dos numeros
-    * @return division entre numeros
-    */
-   public int division(){
-       if (num2<0){
-         System.out.println("Ingrese denominador positivo");
-       }
-       return num1 / num2;      
-   }
-   
-  /**
-    * metodo que devuelve la potencia n de un numero entero
-    * @return resultado de la potencia 
-    */
-public int potencia(){
+    public int suma() {
+        return num1 + num2;
+    }
 
-       
-       if (num2>0){
-          resultado = (int) Math.pow(num1, num2);
-       }
-        else{
+    /**
+     * Funcion que realiza la resta de dos números
+     *
+     * @return devuelve el resultado de la resta
+     */
+    public int resta() {
+        return num1 - num2;
+    }
+
+    /**
+     * metodo que devuelve la multiplicacion entre dos numeros
+     *
+     * @return multiplicacion
+     */
+    public int multiplicacion() {
+        return num1 * num2;
+    }
+
+    /**
+     * metodo que devuelve la division entre dos numeros
+     *
+     * @return division entre numeros
+     */
+    public int division() {
+        if (num2 < 0) {
+            System.out.println("Ingrese denominador positivo");
+        }
+        return num1 / num2;
+    }
+
+    /**
+     * metodo que devuelve la potencia n de un numero entero
+     *
+     * @return resultado de la potencia
+     */
+    public int potencia() {
+
+        if (num2 > 0) {
+            resultado = (int) Math.pow(num1, num2);
+        } else {
 
             System.out.println("Ingrese una potencia positiva");
         }
-    return resultado;
-}
-
- /**
-    * metodo que devuelve la el mod de dos numeros
-    * @return mod entre numeros
-    */
-
-    public int modulo(){
-        return num1%num2;
-    } 
+        return resultado;
     }
+
+    /**
+     * metodo que devuelve la el mod de dos numeros
+     *
+     * @return mod entre numeros
+     */
+    public int modulo() {
+        return num1 % num2;
+    }
+}
