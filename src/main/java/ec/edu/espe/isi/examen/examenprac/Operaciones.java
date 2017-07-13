5149fefd99b43b505278798cdaeaf2739c2e2ab5
Educatespe <educatespe@gmail.com>
2017-07-12 20:11:20 -0500

    Creacion de operaciones suma y resta.

---
+++ src/main/java/ec/edu/espe/isi/examen/examenprac/Operaciones.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.isi.examen.examenprac;
+
+/**
+ *
+ * @author sony
+ */
+public class Operaciones {    
+    private int num1;
+    private int num2;
+
+    public Operaciones(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    
+   public int suma(){
+       return num1 + num2;
+   }
+   
+   public int resta(){
+       return num1 - num2;
+   }
+}
